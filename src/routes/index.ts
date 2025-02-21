a22c4df5b37d74a4ef2c6ddb60fc93ca467d7292
jayjokeer <johntommokaa@gmail.com>
2025-02-21 18:43:43 +0100

    added rides to index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import {AuthRoute} from './authRoutes';
+import { RideRoute } from './rideRoutes';
 
 const router = Router();
 router.use('/auth',  AuthRoute);
+router.use('/rides',  RideRoute );
 export default router;
